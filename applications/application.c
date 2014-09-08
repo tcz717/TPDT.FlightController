c3264e2399378d41e5093405c61d323fb2f39955
tcz717 <tcz717@hotmail.com>
2014-09-08 19:38:47 +0800

    加入了传感器线程
    Signed-off-by: tcz717 <tcz717@hotmail.com>

--- applications/application.c
+++ applications/application.c
@@ -5,6 +5,7 @@
 
 #include "i2c1.h"
 #include "bmp085.h"
+#include "MPU6050.h"   
 
 #ifdef RT_USING_DFS
 /* dfs filesystem:ELM filesystem init */
@@ -15,9 +16,59 @@
 
 #include "led.h"
 
-//ALIGN(RT_ALIGN_SIZE)
-//static rt_uint8_t led_stack[ 512 ];
-//static struct rt_thread led_thread;
+ALIGN(RT_ALIGN_SIZE)
+static rt_uint8_t control_stack[ 512 ];
+static struct rt_thread control_thread;
+
+
+ALIGN(RT_ALIGN_SIZE)
+static rt_uint8_t ahrs_stack[ 128 ];
+static struct rt_thread ahrs_thread;
+static struct rt_event ahrs_event;
+
+ALIGN(RT_ALIGN_SIZE)
+static rt_uint8_t mpu6050_stack[ 128 ];
+static struct rt_thread mpu6050_thread;
+static struct rt_semaphore mpu6050_sem;
+
+void control_thread_entry(void* parameter)
+{
+	
+}
+
+
+void ahrs_thread_entry(void* parameter)
+{
+	rt_uint32_t e;
+	while(1)
+	{
+		rt_sem_release(&mpu6050_sem);
+		
+		if(rt_event_recv(&ahrs_event,1,
+						RT_EVENT_FLAG_AND|RT_EVENT_FLAG_CLEAR,
+						10,&e)==RT_EOK)
+		{
+			
+		}
+	}
+}
+
+void mpu6050_thread_entry(void* parameter)
+{
+	s16 AccelGyro[6];
+	rt_sem_init(&mpu6050_sem,"mpu_t",0,RT_IPC_FLAG_FIFO);
+	MPU6050_SetFullScaleGyroRange(MPU6050_GYRO_FS_1000);
+	MPU6050_SetFullScaleAccelRange(MPU6050_ACCEL_FS_8);
+	while(1)
+	{
+		rt_sem_take(&mpu6050_sem,RT_WAITING_FOREVER);
+		if( MPU6050_TestConnection())
+		{
+			MPU6050_GetRawAccelGyro(AccelGyro);
+		}
+		rt_schedule();
+	}
+}
 
 void rt_init_thread_entry(void* parameter)
 {
@@ -27,6 +78,17 @@ void rt_init_thread_entry(void* parameter)
 	
 	rt_hw_i2c1_init();
 	bmp085_init("i2c1");
+	mpu6050_init("i2c1");
+	
+	rt_event_init(&ahrs_event,"ahrs_e",RT_IPC_FLAG_FIFO);
+	
+	rt_thread_init(&control_thread,
+					"init",
+					control_thread_entry,
+					RT_NULL,
+                    control_stack,
+					512, 8, 20);
+    rt_thread_startup(&control_thread);
 }
 
 int rt_application_init(void)
--- drivers/spi2.c
+++ drivers/spi2.c
@@ -1,3 +1,8 @@
 #include "spi2.h"
 #include "stm32f10x.h"
 #include "board.h"
+
+void rt_hw_spi2_init(void)
+{
+	
+}
